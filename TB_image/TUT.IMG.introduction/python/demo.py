8d855cc179bd207e953ad634c7e0d54404751bda
yann gavet <gavet@emse.fr>
2022-03-22 14:41:07 +0100

    better use of skimage
    uniform filter
    gaussian
    prewitt and sobel
    numpy histogram

--- TB_image/TUT.IMG.introduction/python/demo.py
+++ TB_image/TUT.IMG.introduction/python/demo.py
@@ -7,9 +7,11 @@ Created on Thu Mar 15 11:56:44 2018
 """
 
 import matplotlib.pyplot as plt
-from scipy import misc, ndimage
+from skimage.filters.rank import mean  # mean filter
+from skimage.io import imread, imsave
+import skimage.filters  # prewitt, gaussian
+import skimage.data as data
 
-import imageio
 import numpy as np
 # measure time
 import time
@@ -17,22 +19,22 @@ import time
 ###############################################################################
 # %%
 # load and display images
-ascent = misc.ascent()
+camera = data.camera()
 
 plt.figure(figsize=(10, 3.6))
 
 # first subplot
 plt.subplot(131)
-plt.imshow(ascent)
+plt.imshow(camera)
 
 # second subplot
 plt.subplot(132)
-plt.imshow(ascent, cmap=plt.cm.gray)
+plt.imshow(camera, cmap=plt.cm.gray)
 plt.axis('off')
 
 # third subplot (zoom)
 plt.subplot(133)
-plt.imshow(ascent[200:220, 200:220], cmap=plt.cm.gray, interpolation='nearest')
+plt.imshow(camera[200:220, 200:220], cmap=plt.cm.gray, interpolation='nearest')
 
 plt.subplots_adjust(wspace=0, hspace=0.,
                     top=0.99, bottom=0.01,
@@ -44,11 +46,11 @@ plt.show()
 # %%
 # test jpeg quality
 p=128
-imageio.imwrite("a_25.python.jpeg", ascent[:p, :p], quality=25);
-imageio.imwrite("a_100.python.jpeg", ascent[:p, :p], quality=100);
-imageio.imwrite("a_50.python.jpeg", ascent[:p, :p], quality=50);
-imageio.imwrite("a_75.python.jpeg", ascent[:p, :p], quality=75);
-imageio.imwrite("a_1.python.jpeg", ascent[:p, :p], quality=1);
+imsave("a_25.python.jpeg", camera[p:2*p, p:2*p], quality=25);
+imsave("a_100.python.jpeg", camera[p:2*p, p:2*p], quality=100);
+imsave("a_50.python.jpeg", camera[p:2*p, p:2*p], quality=50);
+imsave("a_75.python.jpeg", camera[p:2*p, p:2*p], quality=75);
+imsave("a_1.python.jpeg", camera[p:2*p, p:2*p], quality=1);
 
 ###############################################################################
 # %%
@@ -60,10 +62,10 @@ def image_stretch(I):
     return I.astype('int')
 
 
-a0 = ascent/4
+a0 = camera/4
 plt.imshow(a0, cmap='gray')
 plt.show()
-a2 = image_stretch(ascent)
+a2 = image_stretch(camera)
 plt.imshow(a2, cmap='gray')
 plt.title('image stretching')
 plt.show()
@@ -72,36 +74,36 @@ plt.show()
 # %%
 # color quantization
 
-q4 = ascent//4 *4
-q16= ascent//16*16
-q32= ascent//32*32
+q4 = camera//4 *4
+q16= camera//16*16
+q32= camera//32*32
 
-imageio.imwrite('q4.python.png', q4);
-imageio.imwrite('q16.python.png', q16);
-imageio.imwrite('q32.python.png', q32);
+imsave('q4.python.png', q4);
+imsave('q16.python.png', q16);
+imsave('q32.python.png', q32);
 
 
 ###############################################################################
 # %%
 # resizing
-S = ascent.shape
+S = camera.shape
 for s in [2,3,4,5,10,20]:
-    r = ascent[::s, ::s]
+    r = camera[::s, ::s]
     plt.imshow(r);
     plt.show()
-    imageio.imwrite('r'+str(s)+'.python.png', r)
+    imsave('r'+str(s)+'.python.png', r)
 
 
 ###############################################################################
 # %%
 # observation of the different color channels
-retine = imageio.imread('retine.png');
+retine = imread('retine.png');
 plt.imshow(retine);
 
 for c in range(3):
     plt.imshow(retine[:,:,c], cmap=plt.cm.gray)
     plt.show()
-    imageio.imwrite('retine_'+str(c)+'.python.png', retine[:,:,c])
+    imsave('retine_'+str(c)+'.python.png', retine[:,:,c])
     
 ###############################################################################
 # %%
@@ -130,66 +132,53 @@ def compute_histogram2(image):
 
 # --------------------
 # beginning of code
-# load ascent and compute histograms
-ascent = misc.ascent()
-t0 = time.clock()
-h = compute_histogram(ascent)
-t1 = time.clock()
-h2 = compute_histogram2(ascent)
-t2 = time.clock()
+# load camera and compute histograms
+camera = data.camera()
+t0 = time.time()
+h = compute_histogram(camera)
+t1 = time.time()
+h2 = compute_histogram2(camera)
+t2 = time.time()
 
 # .... plots
-print("execution time 2D: %g s" % (t1-t0))
+print(f"execution time 2D: {t1-t0:.2} s")
 plt.subplot(131)
 plt.plot(h)
 plt.title('2D function')
 
-print("execution time 1D: %g s" % (t2-t1))
+print(f"execution time 1D: {t2-t1:.2} s")
 plt.subplot(132)
 plt.plot(h2)
 plt.title('1D function')
 
-# last plot: with matplotlib function
+# last plot: with numpy function
 plt.subplot(133)
-t3 = time.clock()
-plt.hist(ascent.flatten(), 256)
-t4 = time.clock()
-print ("execution time matplotlib: %g s" % (t4-t3))
+t3 = time.time()
+h, edges = np.histogram(camera, bins=256)
+plt.plot(edges[:-1], h)
+t4 = time.time()
+print(f"execution time numpy: {t4-t3:.2} s")
 
 plt.savefig("histogrammes.pdf")
 
 # display
 plt.show()
 
-###############################################################################
-# %%
-# aliasing effect (Moiré)
-
-
-def circle(fs, f):
-    # Generates an image with aliasing effect
-    # fs: sample frequency
-    # f : signal frequency
-    t = np.arange(0, 1, 1./fs)
-    ti, tj = np.meshgrid(t, t)
-    C = np.sin(2*np.pi*f*np.sqrt(ti**2 + tj**2))
-    return C
-
-
-C = circle(300, 50)
-plt.imshow(C, cmap=plt.cm.gray)
+# Save histogram on 1 figure
+plt.plot(edges[:-1], h)
+plt.savefig("histogramme.pdf")
 plt.show()
 
-imageio.imwrite('moire.png', C)
+
 
 ###############################################################################
 # %% filtering by convolution: uniform (average), prewitt, gaussian
-# ascent image
-ascent = misc.ascent()
+# camera image
+camera = data.camera()
 
 # mean on a 3x3 neighborhood
-m3 = ndimage.filters.uniform_filter(ascent)
-m25 = ndimage.filters.uniform_filter(ascent, 25)
+m3 = mean(camera, np.ones((3, 3)))
+m25 = mean(camera, np.ones((25, 25)))
 
 plt.subplot(121)
 plt.imshow(m3, cmap=plt.cm.gray)
@@ -203,18 +192,18 @@ plt.title('25x25 mean filter')
 
 plt.show()
 
-imageio.imwrite('ascent_mean_3.png', m3)
-imageio.imwrite('ascent_mean_25.png', m25)
+imsave('camera_mean_3.png', m3)
+imsave('camera_mean_25.png', m25)
 
 # Prewitt filter
-prewitt0 = ndimage.filters.prewitt(ascent, axis=0)
-imageio.imwrite('prewitt0.png', prewitt0)
-prewitt1 = ndimage.filters.prewitt(ascent, axis=1)
-imageio.imwrite('prewitt1.png', prewitt1)
+prewitt0 = skimage.filters.prewitt(camera, axis=0)
+imsave('prewitt0.png', prewitt0)
+prewitt1 = skimage.filters.prewitt(camera, axis=1)
+imsave('prewitt1.png', prewitt1)
 
 # Gaussian filter
-gaussian = ndimage.filters.gaussian_filter(ascent, 5)
-imageio.imwrite('gaussian.png', gaussian)
+gaussian = skimage.filters.gaussian(camera, 5)
+imsave('gaussian.png', gaussian)
 
 # display results
 plt.subplot(131)
@@ -237,11 +226,11 @@ plt.show()
 # using convolution filter
 from scipy.signal import convolve2d
 h = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1] ])
-grad1 = convolve2d(ascent, h);
+grad1 = convolve2d(camera, h);
 plt.imshow(grad1, cmap=plt.cm.gray)
 plt.show()
 
-grad0 = convolve2d(ascent, h.transpose());
+grad0 = convolve2d(camera, h.transpose());
 plt.imshow(grad0, cmap=plt.cm.gray)
 plt.show()
 
@@ -264,7 +253,7 @@ def sharpen(I, alpha):
     return E
 
 
-I = skimage.io.imread('osteoblaste.bmp').astype(np.float)
+I = imread('osteoblaste.png').astype(np.float)
 I = I/255
 A = [1, 0.5, 5]
 
@@ -278,6 +267,27 @@ for i,alpha in enumerate(A):
     plt.imshow(E, cmap=plt.cm.gray)
     plt.title('alpha='+str(alpha))
     
-    skimage.io.imsave('osteoblaste_rehauss_'+str(alpha)+'.python.png', E)
+    imsave('osteoblaste_rehauss_'+str(alpha)+'.python.png', E)
 
 plt.show()
+
+###############################################################################
+# %%
+# aliasing effect (Moiré)
+
+
+def circle(fs, f):
+    # Generates an image with aliasing effect
+    # fs: sample frequency
+    # f : signal frequency
+    t = np.arange(0, 1, 1./fs)
+    ti, tj = np.meshgrid(t, t)
+    C = np.sin(2*np.pi*f*np.sqrt(ti**2 + tj**2))
+    return C
+
+
+C = circle(300, 50)
+plt.imshow(C, cmap=plt.cm.gray)
+plt.show()
+
+imsave('moire.png', C)
