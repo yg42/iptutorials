775b54abeb265795ef32d03f33d8f233760774b4
yann gavet <gavet@emse.fr>
2022-04-12 11:20:06 +0200

    imresize and deconvolution enhancements

--- TB_image/TUT.IMG.image_restoration_deblurring/python/demo.py
+++ TB_image/TUT.IMG.image_restoration_deblurring/python/demo.py
@@ -18,21 +18,51 @@ import progressbar
 def psf2otf(psf, s):
     """
     Get OTF (Optical Transfer Function) from PSF (Point Spread Function)
-    OTF is basically the Fourier Transform of the PSF, centerd
+    OTF is basically the Fourier Transform of the PSF, centered
     psf: PSF 
     s: shape of the result, zero-padding is used to center is Fourier Transform
     """
-    sh = psf.shape
-    sh = np.array(sh)
+    
+    # computes padding values
+    sh = np.array(psf.shape)
     s = np.array(s)
     pad = s - sh
-    h = np.pad(psf, ((0, pad[0]), (0, pad[1])), mode='constant')
-    shift = (int(pad[0]/2+1), int(pad[1]/2+1))
-    h_centered = np.roll(h, tuple(shift), axis=(0, 1))
+    
+    # centers psf
+    h_centered = np.pad(psf, ((pad[0]//2+1, pad[0]-pad[0]//2-1), (pad[1]//2+1, pad[1]-pad[1]//2-1)), mode='constant')
+
+    plt.imshow(h_centered)
+    plt.show()
+    # Fourier transform (aka OTF) of the psf
     h_centered = np.fft.fftshift(h_centered)
     H = np.fft.fft2(h_centered, s)
+    
+    # Keep only real values (simple approximation)
     H = np.real(H)
     return H
+
+# def psf2otf(psf, s):
+#     """
+#     Get OTF (Optical Transfer Function) from PSF (Point Spread Function)
+#     OTF is basically the Fourier Transform of the PSF, centerd
+#     psf: PSF 
+#     s: shape of the result, zero-padding is used to center is Fourier Transform
+#     """
+#     sh = psf.shape
+#     sh = np.array(sh)
+#     s = np.array(s)
+#     pad = s - sh
+#     h = np.pad(psf, ((0, pad[0]), (0, pad[1])), mode='constant')
+#     shift = (int(pad[0]/2+1), int(pad[1]/2+1))
+#     h_centered = np.roll(h, tuple(shift), axis=(0, 1))
+
+#     # Fourier transform (aka OTF) of the psf
+#     h_centered = np.fft.fftshift(h_centered)
+#     H = np.fft.fft2(h_centered, s)
+    
+#     # Keep only real values (simple approximation)
+#     H = np.real(H)
+#     return H
 
 def checkerboard(s=8):
     return np.kron([[1, 0] * 4, [0, 1] * 4] * 4, np.ones((s, s)))
@@ -59,6 +89,8 @@ def addGaussianNoise(I, sigma=1000):
 C = checkerboard()
 psf = np.load('psf_motion.npy')
 Cb = signal.convolve2d(C, psf, boundary='wrap', mode='same')
+from skimage.io import imsave
+imsave("cb_original.png", Cb)
 
 Cbn, noise = addGaussianNoise(Cb)
 plt.imshow(Cbn, cmap='gray')
@@ -68,11 +100,13 @@ plt.imsave("cbn_original.png", Cbn, cmap='gray')
 
 # test reconstruction on noiseless image
 H = psf2otf(psf, C.shape)
+
 G = np.fft.fft2(Cb)
-alpha = 0.001
+alpha = 0
 F = G / (H+alpha)
 fr = np.real(np.fft.ifft2(F))
 plt.imshow(fr)
+
 plt.title('Noiseless (only motion blur) direct reconstruction')
 plt.imsave("cb_reconstruction.png", fr, cmap='gray')
 plt.show()
@@ -81,13 +115,38 @@ plt.show()
 # though really small, the noise prevents a good reconstruction
 H = psf2otf(psf, C.shape)
 G = np.fft.fft2(Cbn)
-alpha = 0.001
+alpha = 0.01
 F = G / (H+alpha)
 fr = np.real(np.fft.ifft2(F))
 plt.imshow(fr)
 plt.title('Noisy (motion blur + noise) direct reconstruction')
 plt.show()
 plt.imsave("cbn_reconstruction.png", fr, cmap='gray')
+
+# test reconstruction on noisy image
+# though really small, the noise prevents a good reconstruction
+# small values
+H = psf2otf(psf, C.shape)
+G = np.fft.fft2(Cbn)
+H[np.abs(H)<=.01] = 1
+F = G / (H)
+fr = np.real(np.fft.ifft2(F))
+plt.imshow(fr)
+plt.title('Noisy (motion blur + noise) direct reconstruction')
+plt.show()
+plt.imsave("cbn_reconstruction_small.png", fr, cmap='gray')
+
+H = psf2otf(psf, C.shape)
+G = np.fft.fft2(Cbn)
+xx, yy = np.meshgrid(range(G.shape[0]), range(G.shape[1]))
+cutoff = (xx-G.shape[0]//2)**2 + (yy-G.shape[1]//2)**2 >= 10**2
+H[np.fft.fftshift(cutoff)] = 1
+F = G / (H)
+fr = np.real(np.fft.ifft2(F))
+plt.imshow(fr)
+plt.title('Noisy (motion blur + noise) direct reconstruction')
+plt.show()
+plt.imsave("cbn_reconstruction_cutoff.png", fr, cmap='gray')
 
 ###############################################################################
 """ Wiener deconvolution on noisy image
@@ -96,7 +155,8 @@ SpecPuissNoise = np.abs(np.fft.fft2(noise))**2
 PuissMoyNoise = np.mean(SpecPuissNoise)
 SpecPuissImageOrig = np.abs(np.fft.fft2(C))**2
 PuissMoyImageOrig = np.mean(SpecPuissImageOrig)
-ratio = PuissMoyNoise/PuissMoyImageOrig
+# ratio = PuissMoyNoise/PuissMoyImageOrig
+ratio = SpecPuissNoise / SpecPuissImageOrig # le ratio dépend des fréquences
 
 Hw = np.conjugate(H)/(np.abs(H)**2+ratio)
 fr = np.fft.ifft2(Hw * np.fft.fft2(Cbn))
@@ -121,6 +181,7 @@ def vca(g, psf, n_iter, beta=.01):
     beta: Jansson parameter
     """
     H = psf2otf(psf, g.shape)
+    H[H < .01] = 1
 
     # init iterations
     fr = g.copy()
@@ -136,6 +197,12 @@ fr = vca(Cbn, psf, 100)
 plt.imshow(fr)
 plt.title('Van Cittert algorithm')
 plt.imsave("cbn_vca.png", np.real(fr), cmap='gray')
+plt.show()
+
+fr = vca(Cb, psf, 1000, beta=1)
+plt.imshow(fr)
+plt.title('Van Cittert algorithm')
+plt.imsave("cb_vca.png", np.real(fr), cmap='gray')
 plt.show()
 
 
@@ -244,16 +311,19 @@ if jupiter:
     hdu_list = fits.open('ic3g01qlq_drz.fits')
     hdu_list.info()
     image = hdu_list[1].data
+    image = np.nan_to_num(image)
+    image = image / np.max(image)
+    image[image < 0] = 0
     plt.imsave("jupiter.png", image, cmap='gray')
 else:
     hdu_list = fits.open('saturn.fits')
     hdu_list.info()
     image = hdu_list[0].data
+    image = np.nan_to_num(image)
+    image = image / np.max(image)
+    image[image < 0] = 0
     plt.imsave("saturn.png", image, cmap='gray')
 
-image = np.nan_to_num(image)
-image = image / np.max(image)
-image[image < 0] = 0
 plt.imshow(image, cmap='gray')
 plt.show()
 
@@ -275,21 +345,7 @@ plt.imshow(imagepsf)
 plt.show()
 
 
-H = psf2otf(imagepsf, image.shape)
-alpha = .01
-G = np.fft.fft2(image)
-H[H == 0] = np.Inf
-F = G / (H+alpha)
-
-fr = np.real(np.fft.ifft2(F))
-plt.imshow(fr)
-plt.title('Direct reconstruction')
-if jupiter:
-    plt.imsave("jupiter_direct.png", fr, cmap='gray')
-else:
-    plt.imsave("saturn_direct.png", fr, cmap='gray')
-
-plt.show()
+###############################################################################
 
 fr = vca(image, imagepsf, 10)
 plt.imshow(fr)
@@ -326,4 +382,27 @@ if jupiter:
     plt.imsave("jupiter_pmap.png", fr, cmap='gray')
 else:
     plt.imsave("saturn_pmap.png", fr, cmap='gray')
+plt.show()
+
+import skimage.restoration
+fr = skimage.restoration.wiener(image, imagepsf, .001)
+plt.imshow(fr)
+if jupiter:
+    plt.imsave("jupiter_wiener.png", fr, cmap='gray')
+else:
+    plt.imsave("saturn_wiener.png", fr, cmap='gray')
+plt.show()
+
+# direct reconstruction
+H = psf2otf(imagepsf, image.shape)
+G = np.fft.fft2(image)
+H[np.abs(H)<=.05] = 1
+F = G / (H)
+fr = np.real(np.fft.ifft2(F))
+plt.imshow(fr)
+plt.title('Direct reconstruction')
+if jupiter:
+    plt.imsave("jupiter_direct.png", fr, cmap='gray')
+else:
+    plt.imsave("saturn_direct.png", fr, cmap='gray')
 plt.show()
